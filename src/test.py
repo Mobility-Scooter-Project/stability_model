f1ff664fc550e13b04e2487bca9b189d59d7e26a
Ruoqi Huang <huangruoqi374@gmail.com>
2023-08-20 23:47:04 -0700

    change test data

--- src/test.py
+++ src/test.py
@@ -9,6 +9,7 @@ OUTPUT_NAME = 'autoencoder_00'
 
 MAX_EPOCHS = 30
 DATA = get_filenames("data")
+TEST_DATA = get_filenames("test_data")
 OPTIONS = {
     "preprocess": [StableFilter(stable_label=0, padding=30)],
     "batchsize": [20, 40],
@@ -17,9 +18,8 @@ OPTIONS = {
     "layer1": [{"units": i*5} for i in range(1, 10)],
 }
 
-number_of_test_file = 4
-stable_test_data = StableFilter(stable_label=0, padding=30).transform(split_data(DATA[:number_of_test_file], 0, 0)[0])
-unstable_test_data = UnstableFilter(stable_label=0, padding=10).transform(split_data(DATA[:number_of_test_file], 0, 0)[0])
+stable_test_data = StableFilter(stable_label=0, padding=30).transform(split_data(TEST_DATA, 0, 0)[0])
+unstable_test_data = UnstableFilter(stable_label=0, padding=10).transform(split_data(TEST_DATA, 0, 0)[0])
 
 SETTINGS = {
     "max_epochs":MAX_EPOCHS,
