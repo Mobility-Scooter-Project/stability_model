6c946a6a2e1acf79d4e72a5dbc67e684deffc9a6
Ruoqi Huang <huangruoqi374@gmail.com>
2023-08-26 17:51:55 -0700

    add the rest

---
+++ src/throwaway_nn/autoencoder_11.py
@@ -0,0 +1,27 @@
+
+'''
+lstm -> lstm -> lstm
+'''
+from keras import Input, layers, Model
+
+TIMESTEPS = 128
+VECTOR_SIZE = 3
+class Encoder_Decoder:
+    def __init__(self, number_of_features):
+        inputs = Input(shape=(TIMESTEPS, number_of_features))
+        x = layers.LSTM(VECTOR_SIZE, return_sequences=True)(inputs)
+        outputs = layers.LSTM(number_of_features, return_sequences=True)(x)
+        self.model = Model(inputs=inputs, outputs=outputs)
+
+    def target_function(self, data):
+        x, y = data
+        return x
+
+OPTIONS = {
+    "batchsize": [40],
+    "timesteps": [TIMESTEPS],
+    "optimizer": ["adam"],
+    "loss": ['mse', 'mae'],
+    "metrics": ['mse'],
+    "layer1": [{"units": i} for i in [1, 2, 4, 8]],
+}
---
+++ src/throwaway_nn/autoencoder_21.py
@@ -0,0 +1,27 @@
+
+'''
+lstm -> lstm -> lstm
+'''
+from keras import Input, layers, Model
+
+TIMESTEPS = 32
+VECTOR_SIZE = 3
+class Encoder_Decoder:
+    def __init__(self, number_of_features):
+        inputs = Input(shape=(TIMESTEPS, number_of_features))
+        x = layers.Conv1D(1, 3, padding="same")(inputs)
+        outputs = layers.LSTM(number_of_features, return_sequences=True)(x)
+        self.model = Model(inputs=inputs, outputs=outputs)
+
+    def target_function(self, data):
+        x, y = data
+        return x
+
+OPTIONS = {
+    "batchsize": [40],
+    "timesteps": [TIMESTEPS],
+    "optimizer": ["adam"],
+    "loss": ['mse', 'mae'],
+    "metrics": ['mse'],
+    "layer1": [{"filters": i} for i in [1, 2, 4, 8]],
+}
