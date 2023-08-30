45fff8589ebaeeb3287f8bae3ad0d5da9fa87ef4
Ruoqi Huang <huangruoqi374@gmail.com>
2023-08-30 00:25:41 -0700

    add fc layer

---
+++ src/throwaway_nn/autoencoder_99.py
@@ -0,0 +1,28 @@
+'''
+conv1d -> repeat vector -> lstm
+'''
+
+from keras import Input, layers, Model
+TIMESTEPS = 64
+VECTOR_SIZE = 10
+class Encoder_Decoder:
+    def __init__(self, number_of_features):
+        inputs = Input(shape=(TIMESTEPS, number_of_features))
+        x = layers.Dense(VECTOR_SIZE)(x)
+        x = layers.Flatten()(x)
+        repeat = layers.RepeatVector(TIMESTEPS)(x)
+        outputs = layers.LSTM(number_of_features, return_sequences=True)(repeat)
+        self.model = Model(inputs=inputs, outputs=outputs)
+
+    def target_function(self, data):
+        x, y = data
+        return x
+
+OPTIONS = {
+    "batchsize": [40],
+    "timesteps": [TIMESTEPS],
+    "optimizer": ["adam"],
+    "loss": ['mse'],
+    "metrics": ['mse'],
+    "layer1": [{"filters": i} for i in [5, 10, 20, 40, 80]],
+}
